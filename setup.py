f25fd1cad074f0701c6e7731fad1f674236cd0a5
Rishikesh Kshirsagar <k.rishikesh.2399@gmail.com>
2026-04-26 17:40:14 -0500

    Phase 2 commit

--- setup.py
+++ setup.py
@@ -3,8 +3,6 @@ from setuptools import find_packages, setup
 setup(
     name="medical-chatbot",
     version="0.1.0",
-    author="Rishikesh Kshirsagar",
-    author_email="k.rishikesh2399@gmail.com",
     packages= find_packages(),
     install_requires=[]
 )
