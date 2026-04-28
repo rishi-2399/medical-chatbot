d8acf4eed2140f35eabcf2f23848bd731d46a292
Rishikesh Kshirsagar <k.rishikesh.2399@gmail.com>
2026-04-28 03:11:33 -0500

    chatbot made fucntional

---
+++ src/helper.py
@@ -0,0 +1,87 @@
+from langchain.document_loaders import PyPDFLoader, DirectoryLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_community.embeddings import HuggingFaceEmbeddings
+from typing import List
+from langchain.schema import Document
+from langchain.embeddings import HuggingFaceEmbeddings
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+import re
+
+#extract the text from pdfs
+
+def load_pdf_files(data):
+    loader = DirectoryLoader(
+        data,
+        glob="*.pdf",
+        loader_cls=PyPDFLoader
+    )
+
+    documents = loader.load()
+    return documents
+
+
+def filter_minimal_docs(docs: List[Document]) -> List[Document]:
+    minimal_docs: List[Document] = []
+    for doc in docs:
+        src = doc.metadata.get("source")
+        minimal_docs.append(
+            Document(
+                page_content=doc.page_content,
+                metadata={"source": src}
+            )
+        )
+    return minimal_docs
+
+def clean_text(text):
+    text = re.sub(r"-\n", "", text)
+    text = re.sub(r"\n+", "\n", text)
+    return text.strip()
+
+def is_useful(text):
+    noise = ["introduction", "appendix", "index", "preface", "contents"]
+    return text and not any(word in text.lower() for word in noise)
+
+
+
+
+#Split the Data into Text Chunks
+
+def create_chunks_from_documents(
+    documents,
+    chunk_size=500,
+    chunk_overlap=20
+):
+    splitter = RecursiveCharacterTextSplitter(
+        chunk_size=chunk_size,
+        chunk_overlap=chunk_overlap
+    )
+
+    chunks = []
+
+    for doc in documents:
+        content = clean_text(doc.page_content)
+
+        if not is_useful(content):
+            continue
+
+        splits = splitter.split_text(content)
+
+        for chunk in splits:
+            chunks.append(Document(
+                page_content=chunk,
+                metadata={
+                "source": doc.metadata.get("source", "unknown"),
+                "page": doc.metadata.get("page", 0),
+                }
+            ))
+
+    return chunks
+
+def download_embedding():
+    
+    #downloading and return the Huggingface embedding model
+    model_name = "sentence-transformers/all-MiniLM-L6-v2"
+    embeddings = HuggingFaceEmbeddings(
+        model_name=model_name
+    )
+    return embeddings
---
+++ store_index.py
@@ -0,0 +1,44 @@
+from dotenv import load_dotenv
+import os
+from pathlib import Path
+from src.helper import load_pdf_files, filter_minimal_docs, create_chunks_from_documents, download_embedding
+from pinecone import Pinecone
+from pinecone import ServerlessSpec 
+from langchain_pinecone import PineconeVectorStore
+from langchain_community.document_loaders import DirectoryLoader, PyPDFLoader
+from langchain_community.embeddings import HuggingFaceEmbeddings
+
+load_dotenv()
+
+PINECONE_API_KEY= os.getenv("PINECONE_API_KEY")
+OPENAI_API_KEY= os.getenv("OPENAI_API_KEY")
+
+os.environ["PINECONE_API_KEY"] = PINECONE_API_KEY
+os.environ["OPENAI_API_KEY"] = OPENAI_API_KEY
+
+data_dir = Path.home() / "Desktop/medical-chatbot/medical-chatbot/data"
+extracted_data = load_pdf_files(str(data_dir))
+filter_data = filter_minimal_docs(extracted_data)
+text_chunks = create_chunks_from_documents(filter_data)
+
+embeddings = download_embedding()
+
+pinecone_api_key  = PINECONE_API_KEY
+pc = Pinecone(api_key=pinecone_api_key)
+
+index_name = "medical-chatbot"  # change if desired
+
+if not pc.has_index(index_name):
+    pc.create_index(
+        name = index_name,
+        dimension=384,
+        metric = "cosine",
+        spec=ServerlessSpec(cloud="aws", region="us-east-1")
+    )
+index = pc.Index(index_name)
+
+docsearch = PineconeVectorStore.from_documents(
+    documents=text_chunks,
+    embedding=embeddings,
+    index_name=index_name
+)
