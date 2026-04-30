37b07f294550a635d1d1eb6d09ece40a9b3aa12c
Rishikesh Kshirsagar <k.rishikesh.2399@gmail.com>
2026-04-30 03:52:19 -0500

    adding everything again

--- app.py
+++ app.py
@@ -67,5 +67,5 @@ def chat():
 
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 8081))
+    port = int(os.environ.get("PORT", 8080))
     app.run(host="0.0.0.0", port=port, debug=False)
