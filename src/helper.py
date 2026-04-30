5270a80be0d100a691033f3e2549e3c1ee92b171
Rishikesh Kshirsagar <k.rishikesh.2399@gmail.com>
2026-04-30 03:26:36 -0500

    chatbot runnig again

--- src/helper.py
+++ src/helper.py
@@ -4,6 +4,7 @@ from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain.schema import Document
 from typing import List
 import re
+from langchain_huggingface import HuggingFaceEmbeddings
 
 
 def load_pdf_files(data):
@@ -63,4 +64,4 @@ def create_chunks_from_documents(documents, chunk_size=500, chunk_overlap=20):
 
 
 def download_embedding():
-    return OpenAIEmbeddings(model="text-embedding-3-small")
+    return HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
